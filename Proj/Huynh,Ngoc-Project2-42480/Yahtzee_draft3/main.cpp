b4cb01b03bfb6b13f11cef99b596dc23dbfb4c02
nh2749275 <nhuynh6@student.rccd.edu>
2018-06-01 16:52:24 -0700

    Signed-off-by: nh2749275 <nhuynh6@student.rccd.edu>

--- Proj/Huynh,Ngoc-Project2-42480/Yahtzee_draft3/main.cpp
+++ Proj/Huynh,Ngoc-Project2-42480/Yahtzee_draft3/main.cpp
@@ -59,14 +59,14 @@ int main(int argc, char** argv) {
     int total, // total score on 5 dice 
         choice, // user chooses when to start game and when to quit game
         dice[6], // dice[0], dice[1], dice[2], dice[3],dice[4], dice[5]
-        numPlrs = 6,// # of players 
+        numPlrs = 3,// # of players 
         plyRoll[numPlrs],
         howmany, // number of dice roll for roll #2 and roll #3
         sumOf[7],// sumOf[1], sumOf[2], sumOf[3],sumOf[4], sumOf[5], sumOf[6]
         sum7, sum8, sum13; //sum of all dice for case 7, 8 and 13
     char roll(char);//player with a value of the dice
     float average; // average of score of 1 person after a certain games
-    string name[6], // name of 3 players
+    string name[3], // name of 3 players
            plyFrst; // the name of the first player
     //Randomize the dice
     srand(static_cast<unsigned int>(time(0)));
@@ -87,8 +87,8 @@ int main(int argc, char** argv) {
     if (choice == 1){
         stdard();
         cout << endl;
+        cout << "Enter names of 3 players" << endl;
         
-         
         do {
             for (int i = 0; i < 6; i++)
                 plyRoll[i] = roll(1);
@@ -113,7 +113,7 @@ int main(int argc, char** argv) {
         // Player with max dice value plays first
         
         cout << "Player #" << maxPlyr << " plays first." << endl;
-        for (int a = 1; a <= 6; a++){
+        for (int a = 1; a <= 3; a++){
             cout << "Player #" << a << endl;
         cout << "You have 3 times to roll" << endl;
         cout << "roll #1 "  << endl;
@@ -220,7 +220,7 @@ int main(int argc, char** argv) {
                     int sum7 = 0; // sum of all dice 
                     int* dcTemp = dice;
                     sort(dcTemp, dcTemp + 6);
-                    int temp = dice[1]; // dice 0 2, 2, 2, 3, 5 // 3,5,2,3, 4
+                    int temp = 0; // dice 0 2, 2, 2, 3, 5 // 3,5,2,3, 4
                     int total7 = 1; //total3 is number of dice that are the same
                     
                     out = true;
@@ -234,13 +234,10 @@ int main(int argc, char** argv) {
                             total7 = 1; // reset number of dices that are the same to 1
                         else { 
                             cout << "Your score is " << sum7 << endl;
-                            break;
                         } 
                         temp = dice[a]; // temp = 2
-                        sum7 += dice[5];
+                        sum7 += dice[a];
                     }
-                        
-                  
                     if (total7 != 3) {
                         cout << "You don't have 3 of a kind. Please choose another field." << endl;
                         cin >> choice;
